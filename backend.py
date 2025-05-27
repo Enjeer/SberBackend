3d36adf6a46bebd02945880e5d20c294e12119f0
NAI - VIIjeer <119804918+Enjeer@users.noreply.github.com>
2025-05-27 21:50:49 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -16,7 +16,7 @@ app.add_middleware(
 )
 
 HF_API_TOKEN = os.getenv("HF_API_TOKEN")
-HF_MODEL = "tiiuae/falcon-1b-instruct"
+HF_MODEL = "EleutherAI/gpt-neo-1.3B"
 
 class Message(BaseModel):
     text: str
